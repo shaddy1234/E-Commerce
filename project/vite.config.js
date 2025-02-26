fc3bc1d44a6fd9a2826d3651b80d65c1d72d30cd
Shadrack Kimaau <musyokishadrach1@gmail.com>
2025-02-27 02:10:31 +0300

    Add files for production

--- project/vite.config.js
+++ project/vite.config.js
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true // Fixes 404 issues with client-side routing
+  }
+});
