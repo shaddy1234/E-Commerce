b150418a7b2d9aa8902db64eba54a72c1e08ca0e
Shadrack Kimaau <musyokishadrach1@gmail.com>
2025-02-26 18:47:09 +0300

    Add Shipping info

---
+++ project/src/pages/ShippingInfo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Truck, HeadphonesIcon, RefreshCw } from 'lucide-react'
+
+function ShippingInfo() {
+  return (
+    <div className='container mx-auto p-8 w-100'>
+        <h1 className='text-3xl font-bold'>Shipping Information</h1>
+         <p className='text-lg'>We offer fast and reliable shipping within 3-5 business days.</p>
+    </div>
+  )
+}
+export default ShippingInfo
