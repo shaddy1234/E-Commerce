63c0df7b05cd7b849ad0f64f223e459202058236
Shadrack Kimaau <musyokishadrach1@gmail.com>
2025-02-26 15:46:51 +0300

    Import footer to App

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import Home from './pages/Home'
 import Products from './pages/Products'
 import Cart from './pages/Cart'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 flex flex-col">
       <Navbar />
-      <main className="container mx-auto px-4 py-8">
+      <main className="container mx-auto px-4 py-8 flex-grow">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
       </main>
+      <Footer />
     </div>
   )
 }
