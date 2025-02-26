aebd816660d9398c84e28504253eaa973d965d93
Shadrack Kimaau <musyokishadrach1@gmail.com>
2025-02-26 15:01:08 +0300

    Import main entry file

---
+++ project/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import Cart from './pages/Cart'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <main className="container mx-auto px-4 py-8">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </main>
+    </div>
+  )
+}
+
+export default App
