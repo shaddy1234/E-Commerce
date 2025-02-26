c84e2546c42956bd1b566a8cc7c7a7ec782cc68e
Shadrack Kimaau <musyokishadrach1@gmail.com>
2025-02-26 18:33:18 +0300

    Update footer

--- project/src/components/Footer.jsx
+++ project/src/components/Footer.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { Facebook, Twitter, Instagram, Mail, Phone, MapPin, Heart } from 'lucide-react'
 
 function Footer() {
+
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const today = new Date();
   return (
     <footer className="bg-gray-800 text-white mt-16">
       <div className="container mx-auto px-4 py-12">
@@ -60,8 +63,20 @@ function Footer() {
           </div>
         </div>
         
-        <div className="border-t border-gray-700 mt-8 pt-8 text-center">
+        {/* <div className="border-t border-gray-700 mt-8 pt-8 text-center">
           <p className="text-gray-300">© 2024 ShopHub. Made with <Heart className="w-4 h-4 inline text-red-500" /> All rights reserved.</p>
+          <p>I wish you a wonderful <span className='text-cyan-700'>{days[today.getDay()]}!</span></p>
+        </div> */}
+        <div className="mt-8 pt-8 border-t border-[var(--border-color)]">
+          <div className="flex flex-col md:flex-row items-center justify-center gap-2">© 2025 ShopHub. 
+            <p className="text-center text-[var(--text-secondary)] flex items-center">
+              Made with <Heart className="w-4 h-4 mx-1 text-red-500" /> by
+              Dev Kim_S ||
+            </p>
+            <p className="text-center text-[var(--text-secondary)]">
+              Have a nice <span className="text-cyan-700">{days[today.getDay()]}!</span>
+            </p>
+          </div>
         </div>
       </div>
     </footer>
